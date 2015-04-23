4c3cf98745a429aa633a32b04c210bff9b4cb0e8
Christoph Witzko <github@christophwitzko.com>
2015-04-23 21:45:02 +0200

    feat: added basic hapi server

---
+++ index.js
@@ -0,0 +1,14 @@
+import Hapi from 'hapi'
+import app from './lib'
+
+var server = new Hapi.Server()
+server.connection({
+  host: 'localhost',
+  port: 3000
+})
+
+app(server)
+
+server.start(() => {
+  console.log('Server running at:', server.info.uri)
+})
