6b36365e54c7e2265d31d83468ded3795a2157f1
Christoph Witzko <github@christophwitzko.com>
2015-04-27 23:45:02 +0200

    chore: export environvent variables

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker info
-docker pull wallpulse/app
-docker stop wallpulse-dev || true
-docker rm wallpulse-dev || true
-docker run -d --name wallpulse-dev --restart=on-failure:3 -e VIRTUAL_HOST=dev.wallpul.se wallpulse/app
-docker rmi $(docker images -q -f dangling=true) || true
+sudo docker info
+sudo docker pull wallpulse/app
+sudo docker stop wallpulse-dev || true
+sudo docker rm wallpulse-dev || true
+sudo docker run -d --name wallpulse-dev --restart=on-failure:3 -e CONSUMER_KEY=$CONSUMER_KEY -e CONSUMER_SECRET=$CONSUMER_SECRET -e ACCESS_TOKEN=$ACCESS_TOKEN -e ACCESS_TOKEN_SECRET=$ACCESS_TOKEN_SECRET -e VIRTUAL_HOST=dev.wallpul.se wallpulse/app
+sudo docker rmi $(sudo docker images -q -f dangling=true) || true
